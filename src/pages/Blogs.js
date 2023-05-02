584199a0796bddc64f5157ad39e7dd414d700001
Andres Järviste <andres.jarviste@wbscodingschool.com>
2023-05-02 19:37:26 +0300

    uudised

---
+++ src/pages/Blogs.js
@@ -0,0 +1,10 @@
+function Blogs() {
+    return (
+        <div className="home">
+            <h1>Blogipostitused</h1>
+        </div>
+    )
+
+}
+
+export default Blogs
---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <div className="home">
+            <h1>Kontakt</h1>
+        </div>
+    )
+
+}
+
+export default Contact
