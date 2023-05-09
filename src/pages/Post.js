32463a55760c57e7010dc286b68f5a6b7a7abf4e
Andres Järviste <andres.jarviste@wbscodingschool.com>
2023-05-09 18:15:08 +0300

    parandused

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,22 +1,8 @@
 import { useParams } from "react-router-dom";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 function Post({postitused}) {
     let {postId} = useParams();
-
-    const renderOption = {
-        renderNode: {
-          [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-            return (<img
-              src={`https://${node.data.target.fields.file.url}`}
-              height={node.data.target.fields.file.details.image.height}
-              width={node.data.target.fields.file.details.image.width}
-              alt="??"
-            />)
-          }
-        }
-      }
      
     const postData = postitused.find(post => postId === post.id)
     return (
